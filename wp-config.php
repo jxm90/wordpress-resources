bc27f00b1b0d63c31d786fa3c6e97496054b79d5
jxm90 <joemax1990@gmail.com>
2024-02-19 12:15:22 -0600

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,10 @@
+define( 'WPMS_ON', true ); // True turns on constants support and usage, false turns it off.
+define( 'WPMS_SMTP_HOST', 'smtp.emailarray.com' ); // The SMTP mail host.
+define( 'WPMS_SMTP_PORT', 587 ); // The SMTP server port number.
+define( 'WPMS_SSL', 'tls' ); // Possible values '', 'ssl', 'tls' - note TLS is not STARTTLS.
+define( 'WPMS_SMTP_AUTH', true ); // True turns it on, false turns it off.
+define( 'WPMS_SMTP_USER', 'from_email' ); // SMTP authentication username, only used if WPMS_SMTP_AUTH i>
+define( 'WPMS_SMTP_PASS', 'password' ); // SMTP authentication password, only used if WPMS_SMTP_AUTH is true.
+define( 'WPMS_SMTP_AUTOTLS', true ); // True turns it on, false turns it off.
+define( 'WPMS_MAILER', 'smtp' );
+define( 'WPMS_MAIL_FROM', 'from_email' );
